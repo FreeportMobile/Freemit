962562fdaedb839803c8ea7a5ece23a0129c981c
@bkawk <email.will.in.china@gmail.com>
2016-04-28 11:28:01 +0800

    Currency mapping to dialing code

--- helpers/mongo.js
+++ helpers/mongo.js
@@ -3,6 +3,48 @@
 //-- SETUP MONGO
 var mongoClient = require('mongodb').MongoClient;
 
+//----------------------- SET COUNTRY CODE ----------------------------------//
+
+exports.setCountryCode = function (country_name, currency_symbol, currency_abbreviation, dialing_code) {
+    return new Promise(function(resolve, reject) {
+
+        // OPEN CONNECTION     
+        mongoClient.connect(process.env.MONGO_DB, function (err, db) {
+            if (err) {
+                reject(err);
+                console.log('Unable to connect to the mongoDB server. Error:', err);
+            } 
+
+            // PREPARE DATA
+            var collection = db.collection('country_codes');
+            var doc = {
+                country_name: country_name,
+                currency_symbol: currency_symbol,
+                currency_abbreviation: currency_abbreviation,
+                dialing_code: dialing_code
+            };
+         
+         
+        // UPDATE   
+        
+        collection.insert(doc, {w:1}, function(err, result) {
+           
+            if(err){
+                reject(err);
+            }else{
+                resolve(result);
+            }
+            db.close();
+            
+        });
+        
+   
+
+
+        }); //-- END CONNECT      
+    }); //-- END PROMISE
+}; //-- END FUNCTION
+
 //----------------------- SET SMS ----------------------------------//
 
 exports.setSMS = function (phoneNumber, verificationCode) {
