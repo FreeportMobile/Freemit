f6ad5463180e7b6efc80f7ac03f96d61a67a09c6
@bkawk <email.will.in.china@gmail.com>
2016-05-02 09:18:19 +0800

    expand rate limiter

--- sockets/events.js
+++ sockets/events.js
@@ -4,6 +4,9 @@ module.exports = function (io) {
 
 //-- CONNECT 
     io.on('connect', function (socket, msg) {
+       
+//-- SET LAST ACTIVITY FOR RATE LIMITING        
+       socket.lastAcivity = new Array(Date.now().toString());
 
 //-- MAKE CONTROLLERS AVAILABLE
         var freemit = require('./freemit');
@@ -13,32 +16,26 @@ module.exports = function (io) {
 
 //-- SOCKET EVENTS
         socket.on('sendVerificationCode', function (msg) {
-            rateLimit(100);
+            rateLimiter(socket);
             freemit.user.sendVerificationCode(socket, io, msg);
         });
         
         socket.on('checkVerificationCode', function (msg) {
-            rateLimit(100);
+            rateLimiter(socket);
             freemit.user.checkVerificationCode(socket, io, msg);
         });
         
         socket.on('saveCard', function (msg) {
-            rateLimit(100);
+            rateLimiter(socket);
             freemit.user.saveCard(socket, io, msg);
         });
         
         socket.on('getBalance', function (msg) {
-            rateLimit(100);
+            rateLimiter(socket);
             freemit.user.getBalance(socket, io, msg);
         });
         
-        function rateLimit(max){
-            if(Date.now() - socket.requested < max){
-                socket.disconnect();
-                return;
-            }
-        }
-        socket.requested = Date.now();
+
         
 //-- DISCONNECT
         socket.on('disconnect', function(socket) {
@@ -48,3 +45,32 @@ module.exports = function (io) {
 
     }); //-- END CONNECT
 }; //-- END EXPORT
+
+   function rateTracker(socket){
+      // MAKE AN ARRAY THAT IS NOT UNDEFINED
+            if (socket.lastActivity == undefined) {
+                socket.lastActivity = [];
+            }
+     // DONT ALLOW THE ARRAY TO GET LONGER THAN 5
+            if (socket.lastActivity.length > 5) {
+                socket.lastActivity.shift();
+            }
+     // PUSH IN THE DATE TIME NOW
+            socket.lastActivity.push(Date.now().toString());
+        }
+
+        function rateLimiter(socket){
+            // CALL RATE TRACKER
+            rateTracker(socket);
+            // MAKE SURE THE ARRAY IS FULL
+            if (socket.lastActivity.length < 5){
+               return;
+            }
+            // OF THE TIME NOW MINUS FIRST ITEM IN ARRAY IS LESS THAN 200ms DISCONNECT
+            if(parseInt(Date.now()) -parseInt(socket.lastAcivity[0]) < 200){
+            // DISCONNECT JUST THE SOCKET THAT IS FLOODED
+               socket.disconnect();
+               console.log('--- DISCONNECTD - SOCKET FLOODED');
+               return;
+            }
+        }
