440126efb468101bf109b1908085fc0453568df4
@bkawk <email.will.in.china@gmail.com>
2016-05-11 20:36:24 +0800

    remove bank and exchange

--- sockets/exchange.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-//--- ADD EXCHANGE
-exports.add = function (socket, io, msg) {
-    console.log('Add Exchange');
-    io.to(socket.id).emit('addexchange', {msg: 200});
-};
--- sockets/freemit.js
+++ sockets/freemit.js
@@ -1,9 +1,4 @@
 'use strict';
 
 var user = require('./user.js');
-var bank = require('./bank.js');
-var exchange = require('./exchange.js');
-
 exports.user = user;
-exports.bank = bank;
-exports.exchange = exchange;
