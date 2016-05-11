e65e88d400e44240577319c9804cc7d1dc12d5de
@bkawk <email.will.in.china@gmail.com>
2016-05-11 19:49:39 +0800

    pass address

--- helpers/colu.js
+++ helpers/colu.js
@@ -28,90 +28,42 @@ exports.makeAddress = function () {
 exports.getAssets = function (bitcoinAddress) {  // Get Balance of Public Bitcoin Address
     return new Promise(function (fulfill, reject) { // Create Promise
            
-    var settings = {
-	    "network": "mainnet",
-	    "privateSeed": process.env.COLU_PRIVATE_KEY,
-	    "apiKey": process.env.COLU_APIKEY
-    }
+        var settings = {
+	        "network": "mainnet",
+	        "privateSeed": process.env.COLU_PRIVATE_KEY,
+	        "apiKey": process.env.COLU_APIKEY
+        }
 
-    var args = 	{
-	    "address": "1pDbopMHNR1vNgUMrFPp4aduKkoqwxgQq"
-    }
+        var args = 	{
+	        "address": bitcoinAddress
+        }
 
-    var colu = new Colu(settings)
-        colu.on('connect', function () {
-	        colu.coloredCoins.getAddressInfo(args.address, function (err, body) {
-		        if (err) {
-                    reject(err);
-			    return;
-		        }
-                var total = 0;  
-                for (var i = 0; i < body.utxos.length; i++) {  // Iterate through all bitcoin transactions
-                    var assetId = body.utxos[i].assets[0].assetId;  // grab Colored coin AssetId
-                    var amount = body.utxos[i].assets[0].amount;  // Get Amount of Assets
-                    
-                    if (assetId == process.env.COLU_USD) {  // Filter out non Freemit Assets
-                        total = total + amount;  //  Add to totals 
-                    }
-                } // End loop
+        var colu = new Colu(settings)
+            colu.on('connect', function () {
+	            colu.coloredCoins.getAddressInfo(args.address, function (err, body) {
+		            if (err) {
+                        reject(err);
+			        return;
+		            }
+                    var total = 0;  
+                    for (var i = 0; i < body.utxos.length; i++) {  // Iterate through all bitcoin transactions
+                        var assetId = body.utxos[i].assets[0].assetId;  // grab Colored coin AssetId
+                        var amount = body.utxos[i].assets[0].amount;  // Get Amount of Assets
+           
+                        if (assetId == process.env.COLU_USD) {  // Filter out non Freemit Assets
+                            total = total + amount;  //  Add to totals 
+                        }
+                    } // End loop
                 
-                var results = {
-                    currencyAbbreviation: "USD",
-                    total: total,
+                    var results = {
+                        currencyAbbreviation: "USD",
+                        total: total,
                     };
                     
-                    console.log(results);
                     fulfill(results);
-                    
-	        }) // END COLOURED COINS
-        }) // END COLU
-    colu.init()
-
-        
-        // var settings = {
-        //     network: 'mainnet',
-        //     privateSeed: process.env.COLU_PRIVATE_KEY,
-        //     apiKey: process.env.COLU_APIKEY,     
-        // }
-        // var args = 	{
-	    //     "address": bitcoinAddress
-        // }
-        
-  
-        // var colu = new Colu(settings);
-        // console.log('in colu');
-        // colu.on('connect', function () {  //  Once connected perform function 
-        //       console.log('in colu connect');
-        //     colu.coloredCoins.getAddressInfo(bitcoinAddress, function (err, body) {
-        //           console.log(body);
-        //         if (err) {
-        //               console.log('in error');
-        //              console.log(err);
-        //             reject(err); // Return promise rejected
-        //             return console.error(err);
-        //         }
-        //         else {
-        //             console.log('in else');
-        //             var total = 0;  // Start with 0
-        //              console.log(total);
-        //             for (var i = 0; i < body.utxos.length; i++) {  // Iterate through all bitcoin transactions
-        //                 var assetId = body.utxos[i].assets[0].assetId;  // grab Colored coin AssetId
-        //                 var amount = body.utxos[i].assets[0].amount;  // Get Amount of Assets
-        //                 if (assetId == process.env.COLU_USD) {  // Filter out non Freemit Assets
-        //                     total = total + amount;  //  Add to totals 
-        //                 }
-        //             } // End loop
-                    
-        //             var results = {
-        //                 currencyAbbreviation: "USD",
-        //                 total: total,
-        //             };
-        //             console.log(results);
-        //             fulfill(results);
-        //         }
-        //     })
-        //     colu.init();  
-        // }) // End Colu
+	            }) // END COLOURED COINS
+            }) // END COLU
+        colu.init()
     }); // End Promise
 }; // End Function
 
