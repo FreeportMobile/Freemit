0da7440b33be54c288fbd2ace79746f84eac52cb
@bkawk <email.will.in.china@gmail.com>
2016-05-12 19:02:12 +0800

    cool try 2

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 //-- MAKE DEPENDANCIES AVAILABLE
 require('dotenv').config();
-var Colu = require('colu')
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -19,46 +18,58 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     
- //   var privateSeed = '09357bde26559b424a1c50b7228aa7283b6d36d277e9b8d3d2f9a8f3fae20963'
-    var assetId = 'La48eD69AVoYTBbpeX5XD8QKbFnZRwNw6KkcgP'
-    var fromAddress = '16iFLp6znGKxA5LrTPVVVmNbBDcQe7vKqz'
     
-    var privateSeed = colu.hdwallet.getPrivateSeed();
-    var address = colu.hdwallet.getAddress();
-    var toAddress = 'mhiwFNRAbDyyhQi1967qtxjqwiS9R8xuWs';
- //   var toAddress = colu.hdwallet.getAddress();
     
-    var args = {
-        from: [fromAddress],
-        to: [{
-            address: toAddress,
-            assetId: assetId,
-            amount: 2
-            }]
-        }
     
-    colu.sendAsset(args, function (err, body) {
-            res.status(200).json({ 
-                seed: privateSeed,
-                address: address,
-                toAddress: toAddress,
-                body: body,
-                err: err,
-            }); 
+    var Colu = require('colu')
+var settings = {
+    coloredCoinsHost: 'https://testnet.api.coloredcoins.org',
+    coluHost: 'https://testnet.engine.colu.co',
+    network: 'testnet',
+    privateSeed: '09357bde26559b424a1c50b7228aa7283b6d36d277e9b8d3d2f9a8f3fae20963'
+}
+var colu = new Colu(settings)
+
+var assetId = 'La48eD69AVoYTBbpeX5XD8QKbFnZRwNw6KkcgP';
+var fromAddress = '16iFLp6znGKxA5LrTPVVVmNbBDcQe7vKqz';
+var toAddress = 'mhiwFNRAbDyyhQi1967qtxjqwiS9R8xuWs';
+
+var send = {
+    from: fromAddress,
+    to: [{
+        address: toAddress,
+        assetId: assetId,
+        amount: 1
+    }],
+    metadata: {        
+        'assetName': '1 Ticket to see the Chicago Musical on 1/1/2016 at 8 PM',
+        'issuer': 'Ticket booth on Times Square',
+        'description': 'Seat 12 at row 10'
+    }
+};
+
+colu.on('connect', function () {
+    colu.sendAsset(send, function (err, body) {
+        if (err) return console.error(err)        
+        console.log("Body: ",body)
     })
+})
+
+colu.init()
     
+
 });
 
 //--SOCKET EVENTS
 exports.io = require('./sockets/events.js')(io);
 
 //--START SERVER
-colu.on('connect', function () {
+
     server.listen(process.env.WEB_PORT, function () {
         console.info('--- STARTED ---');
     });
-})
-colu.init()
+
+
 
 
 
