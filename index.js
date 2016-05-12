71f3872fa9e0134c1018cc1b1a66bc3a747d7294
@bkawk <email.will.in.china@gmail.com>
2016-05-12 17:22:05 +0800

    cool added

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 
 //-- MAKE DEPENDANCIES AVAILABLE
 require('dotenv').config();
+var Colu = require('colu')
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 
+var colu = new Colu({
+    network: 'testnet',
+    privateSeed: null
+})
 
 //-- MAKE STATIC FILES AVAILABLE
 app.use(express.static(__dirname + '/public')); 
@@ -22,10 +27,13 @@ app.get('/', function (req, res) {
 exports.io = require('./sockets/events.js')(io);
 
 //--START SERVER
+colu.on('connect', function () {
+    server.listen(process.env.WEB_PORT, function () {
+        console.info('--- STARTED ---');
+    });
+})
+colu.init()
 
-server.listen(process.env.WEB_PORT, function () {
-     console.info('--- STARTED ---');
-});
 
 
 
