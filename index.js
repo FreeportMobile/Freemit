3387a3ba27cc92e0779099681ece7038abc0aa3d
@bkawk <email.will.in.china@gmail.com>
2016-05-12 21:33:33 +0800

    issue asset

--- index.js
+++ index.js
@@ -11,12 +11,58 @@ var io = require('socket.io')(server);
 //-- MAKE STATIC FILES AVAILABLE
 app.use(express.static(__dirname + '/public')); 
 
+
+//------------------------- NEW ADDRESS ---------------------
 app.get('/newAddress', function (req, res) {
     var key = bitcoin.ECKey.makeRandom();
-    var address = key.pub.getAddress(bitcoin.networks.mainnet).toString();
+    var address = key.pub.getAddress(bitcoin.networks.testnet).toString();
     var wif = key.toWIF();
     res.status(200).json({ address: address, privateKey: wif });
 });
+
+
+//------------------------- ISSUE ASSET ---------------------
+app.get('/issueAsset', function (req, res) {
+    funded_address = 'mm3dtEPHfghf7P7AsCCnJGV3RS3dSJE9dN'
+    var asset = {
+        'issueAddress': funded_address,
+        'amount': 1,
+        'fee': 5000
+    };
+
+    postToApi('issue', asset, function(err, body){
+        if (err) {
+            console.log('error: ', err);
+        }
+    });
+    
+});
+
+
+//------------------------- SHARED FUNCTIONS ---------------------
+
+function postToApi(api_endpoint, json_data, callback) {
+    console.log(api_endpoint+': ', JSON.stringify(json_data));
+    request.post({
+        url: 'http://testnet.api.coloredcoins.org:80/v3/'+api_endpoint,
+        headers: {'Content-Type': 'application/json'},
+        form: json_data
+    }, 
+    function (error, response, body) {
+        if (error) {
+            return callback(error);
+        }
+        if (typeof body === 'string') {
+            body = JSON.parse(body)
+        }
+        res.status(response.statusCode).json({ body: JSON.stringify(body) });
+        return callback(null, body);
+    });
+};
+
+
+
+
 
 
 
