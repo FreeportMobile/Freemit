7d3ea9ff44ec72a4f5ded11dce3fed8150976af6
@bkawk <email.will.in.china@gmail.com>
2016-05-18 18:10:10 +0800

    utxos

--- helpers/blockchain.js
+++ helpers/blockchain.js
@@ -35,7 +35,7 @@ exports.queryAddress = function (bitcoinAddress) {
                 reject(err);
             }
             if (typeof body === 'string') {
-                body = JSON.parse(body.utos);
+                body = JSON.parse(body.utxos);
                 console.log("BODY");
             }
             resolve(body);   
