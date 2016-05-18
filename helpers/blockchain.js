2476c99f174d2ecece13c7e8e9773ebc75efca10
@bkawk <email.will.in.china@gmail.com>
2016-05-18 17:29:14 +0800

    main api

--- helpers/blockchain.js
+++ helpers/blockchain.js
@@ -60,7 +60,7 @@ console.log(fromAddressArray);
             'to': [{'address': toAddress, 'amount': amount, 'assetId': assetID }]
         };
         request.post({
-            url: 'http://'+process.env.BITCOIN_NET+'.api.coloredcoins.org:80/v3/sendasset',
+            url: 'http://api.coloredcoins.org:80/v3/sendasset',
             headers: {'Content-Type': 'application/json'},
             form: send_asset
         }, 
@@ -90,9 +90,9 @@ console.log(fromAddressArray);
             } //-- END LOOP
             var signedTxHex = tx.toHex();
             var transaction = {'txHex': signedTxHex };
-            // MAKE POST 2        
+            // MAKE POST 2       
             request.post({
-                url: 'http://'+process.env.BITCOIN_NET+'.api.coloredcoins.org:80/v3/broadcast',
+                url: 'http://api.coloredcoins.org:80/v3/broadcast',
                 headers: {'Content-Type': 'application/json'},
                 form: transaction
             }, 
