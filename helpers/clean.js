3629f4db44047adfd1d34300638b4a28e67dd75f
@bkawk <email.will.in.china@gmail.com>
2016-05-24 21:21:43 +0800

    no promises all round

--- helpers/clean.js
+++ helpers/clean.js
@@ -7,17 +7,13 @@ var phone = require('phone');
 
 //----------------------- CLEAN ME ----------------------------------//
 exports.me = function (item) {
-    return new Promise(function (resolve, reject) {
         var cleanedResult = xss(item);        
         resolve(cleanedResult);  
-    }); //-- END PROMISE
 }; //-- END FUNCTION
 
 //----------------------- CLEAN NUMBER ----------------------------------//
 exports.num = function (num) {
-    return new Promise(function (resolve, reject) {
         var cleanedPhone = phone(num);
         resolve(cleanedPhone[0]); 
-    }); //-- END PROMISE
 }; //-- END FUNCTION
 
