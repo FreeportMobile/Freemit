7195569a8df72dff449d474ca24b699631d56760
@bkawk <email.will.in.china@gmail.com>
2016-05-27 20:02:41 +0800

    back to 5000

--- helpers/blockchain.js
+++ helpers/blockchain.js
@@ -51,8 +51,7 @@ fromAddressArray.push(fromAddress);
 
         var send_asset = {
             'from': fromAddressArray,
-            'fee': 6000,  
-            'value': 10000,                                           
+            'fee': 5000,                                        
             'to': [{'address': toAddress, 'amount': amount, 'assetId': assetID }]
         };
         request.post({
