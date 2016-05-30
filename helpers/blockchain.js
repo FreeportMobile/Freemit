5e72d133bb41466c1d84f5bf877dc98b4549c957
@bkawk <email.will.in.china@gmail.com>
2016-05-30 16:06:13 +0800

    just key pair

--- helpers/blockchain.js
+++ helpers/blockchain.js
@@ -10,20 +10,28 @@ exports.sendMessage = function () {
     return new Promise(function(resolve, reject) {    
         
         console.log('SEND MSG 3a');
-        
-        var privateKey = bitcoin.ECKey.fromWIF("KwFmrKDWXT6oNKQ9QosHTU3Vpp6EPT9ExVbq7n8Rq613ARsSr8vs");
-        var keyPair = bitcoin.ECPair.fromWIF('KwFmrKDWXT6oNKQ9QosHTU3Vpp6EPT9ExVbq7n8Rq613ARsSr8vs', bitcoin.networks.mainnet);
-        
-        var fromAddress =   '16WBguy6KVyTGnF4KX7Vmdx8ztj4wENh4W';
-        var toAddress =     '1KAo4aY64FPYMsGdR3SQTXEcxPVqhjQG19';
-        var message =       'Blockchain Chat';
-        
         var net = process.env.BITCOIN_NET;
-        var tx = new bitcoin.TransactionBuilder();
-        tx.addInput(fromAddress, 0)
-        tx.addOutput(toAddress, 1000)
-        tx.sign(0, keyPair);
-        console.log(tx.build().toHex());
+       // var privateKey = bitcoin.ECKey.fromWIF("KwFmrKDWXT6oNKQ9QosHTU3Vpp6EPT9ExVbq7n8Rq613ARsSr8vs");
+        var keyPair = bitcoin.ECPair.fromWIF('KwFmrKDWXT6oNKQ9QosHTU3Vpp6EPT9ExVbq7n8Rq613ARsSr8vs', net);
+        console.log(keyPair);
+        
+        // var fromAddress =   '16WBguy6KVyTGnF4KX7Vmdx8ztj4wENh4W';
+        // var toAddress =     '1KAo4aY64FPYMsGdR3SQTXEcxPVqhjQG19';
+        // var message =       'Blockchain Chat';
+        
+        
+        // var tx = new bitcoin.TransactionBuilder();
+        // tx.addInput(fromAddress, 0)
+        // tx.addOutput(toAddress, 1000)
+        // tx.sign(0, keyPair);
+        // console.log(tx.build().toHex());
+        
+        
+        
+        
+        
+        
+        
         
         //var keyPair = bitcoin.ECPair.fromWIF('KwFmrKDWXT6oNKQ9QosHTU3Vpp6EPT9ExVbq7n8Rq613ARsSr8vs');
         
