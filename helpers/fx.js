9fd48bb8e929e555bacfca0829c7130cc2c01bd3
@bkawk <email.will.in.china@gmail.com>
2016-06-27 18:06:50 +0800

    resolve

--- helpers/fx.js
+++ helpers/fx.js
@@ -3,14 +3,14 @@ var request = require('request');
 //------------------------------------------------- FX NOW --------------------------
 exports.exchange = function (fromCurrency, toCurrency, amount) {
     console.log('FIRE EXCHANGE 1');
-    return new Promise(function (fulfill, reject) { // Create Promise
+     return new Promise(function(resolve, reject) {
         var url = 'http://api.fixer.io/latest?base=' + fromCurrency;
         request(url, function (err, result, body) {
             console.log('FIRE EXCHANGE 2');
             var result = JSON.parse(body);
             if (result.rates[toCurrency]) {
                 console.log('FIRE EXCHANGE 3');
-                fulfill(result.rates[toCurrency])
+                resolve(result.rates[toCurrency])
             }
             else {
                 reject("Not Found");
@@ -20,19 +20,19 @@ exports.exchange = function (fromCurrency, toCurrency, amount) {
 };
 //------------------------- CONVERT UN TO CURRENCY --------------------------
 exports.currency = function (un) {
-    return new Promise(function (fulfill, reject) { // Create Promise
+     return new Promise(function(resolve, reject) {
 
         if (un == 'USA'){
             var currency = 'USD';
-            fulfill(currency)
+            resolve(currency)
         }
         if (un == 'IND'){
             var currency = 'INR';
-            fulfill(currency)
+            resolve(currency)
         }
         if (un == 'CHN'){
             var currency = 'CNY';
-            fulfill(currency)
+            resolve(currency)
         }
         if (un != 'USA' || un != 'IND' || un != 'CHN' ){
             reject("No UN Found");
