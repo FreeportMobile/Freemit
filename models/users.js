ffcd358bc91a15b997aa80cc9c4aa4c8b324a70a
hitsnorth <hitsnorth@hotmail.com>
2016-06-27 13:12:15 -0400

    Mongoose users model

--- models/users.js
+++ models/users.js
@@ -1,4 +1,6 @@
+// Setup Mongoose
 var mongoose = require('mongoose');
+//  Mongoose Schema that defines the user
 var User = new mongoose.Schema({
         name: String,
         phone_number: String,
@@ -6,11 +8,12 @@ var User = new mongoose.Schema({
         bitcoin_address: String,
         private_key: String,
     },
-    {timestamps: true });
+    {timestamps: true});
 //console.log(mongoose.connection.readyState);
-
+// Create connection if its not alreayd instantiated as a singleton
 if (!mongoose.connection.readyState) {
     mongoose.connect(process.env.MONGO_DB);
 }
+// Setup module to export the model
 var users = mongoose.model('users', User);
 module.exports = users;
