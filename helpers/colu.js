d0ea64e3f3d69766d9145869854ff64e3fb57b85
@bkawk <email.will.in.china@gmail.com>
2016-06-28 18:21:27 +0800

    log time

--- helpers/colu.js
+++ helpers/colu.js
@@ -36,21 +36,27 @@ exports.getBallence = function(address){
 
 
 exports.transferFunds = function(fromAddress, amount, toAddress, privateKey, currency, fromPhone, toPhone){
-    return new Promise(function(resolve, reject) {   
-            console.log(' ------------ START SENDING ------------'); 
-            console.log('From Address: '+fromAddress);
-            console.log('Amount: '+amount);
-            console.log('To Address: '+toAddress);
-            console.log('Private Key: '+privateKey);
-            console.log('Currency: '+currency);
-            console.log('From Phone: '+fromPhone);
-            console.log('To Phone: '+toPhone);
-    request('http://coinoutlet.biz:4700/sendCurrency?fromAddress='+fromAddress+'&amount='+amount+'&toAddress='+toAddress+'&privateKey='+privateKey+'&currency='+currency+'&fromPhone='+fromPhone+'&toPhone='+toPhone, function (error, response, body) {
+    return new Promise(function(resolve, reject) { 
+        var startDate = new Date();
+        var startTime = startDate.getTime();
+        console.log(' ------------ START SENDING ------------'); 
+        console.log('From Address: '+fromAddress);
+        console.log('Amount: '+amount);
+        console.log('To Address: '+toAddress);
+        console.log('Private Key: '+privateKey);
+        console.log('Currency: '+currency);
+        console.log('From Phone: '+fromPhone);
+        console.log('To Phone: '+toPhone);
+        request('http://coinoutlet.biz:4700/sendCurrency?fromAddress='+fromAddress+'&amount='+amount+'&toAddress='+toAddress+'&privateKey='+privateKey+'&currency='+currency+'&fromPhone='+fromPhone+'&toPhone='+toPhone, function (error, response, body) {
         if(error){
             reject(error);  
         }
         if (!error && response.statusCode == 200) {
             console.log(' ------------ END SENDING ------------'); 
+            var EndDate = new Date();
+            var EndTime = EndDate.getTime();
+            var FetchTime = EndTime - startTime;
+            console.log('Time to fetch: '+ FetchTime);
             resolve(body);
         }
     })
