c0497dbaf49c54ee0f8c53f1dc685047f5cb27a7
hitsnorth <hitsnorth@hotmail.com>
2016-07-03 21:34:38 -0400

    Error Handling for chats.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var mongoose = require('mongoose');
 
 // -- Setup Mongoose Singleton
 if (!mongoose.connection.readyState) {
