9f53f4f126970243199d12562f934e6cf65db86c
@bkawk <email.will.in.china@gmail.com>
2016-07-04 11:37:37 +0800

    tidy complete

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-// var mongoose = require('mongoose');
-
-// // -- Setup Mongoose Singleton
-// if (!mongoose.connection.readyState) {
-//     mongoose.connect(process.env.MONGO_DB);
-// }
-
 
 //-- MAKE STATIC FILES AVAILABLE
 app.use(express.static(__dirname + '/public')); 
